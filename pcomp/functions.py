20c1643e6b5eb79671a891204062fd620dab25e1
Antonis Piloridis <antonispiloridis@gmail.com>
2024-03-19 01:16:39 +0200

    1.3

--- pcomp/functions.py
+++ pcomp/functions.py
@@ -1,35 +1,66 @@
 from collections.abc import Iterable
 
-def check_arguments(iterable,length):
+def valid_arguments(iterable,length):
+    """
+    Check if the arguments are valid.
+
+    Parameters:
+        iterable (Iterable): The iterable object to be checked.
+        length (int): The length to be checked.
+
+    Raises:
+        TypeError: If iterable is not an Iterable or length is not an Integer or length is not greater than 0.
+    """
     if not(isinstance(iterable, Iterable)):
-        raise ValueError("Iterable Argument is not Iterable.")
+        raise TypeError("Iterable Argument is not Iterable")
     if not(isinstance(length, int)):
-        raise ValueError("Length Argument is not a number.")
+        raise TypeError("Length Argument is not Integer")
     if not(length>0):
-        raise ValueError("Length Argument must be >0 .")
+        raise TypeError("Length Argument must be >0")
+
 
 def area(iterable,length):
-    check_arguments(iterable,length)
-    return pow(len(iterable),length)
+    """
+    Calculate the area for permutation computation.
+
+    Parameters:
+        iterable (Iterable): The iterable object.
+        length (int): The length for permutation computation.
+
+    Returns:
+        int: The area for permutation computation.
+    """
+    valid_arguments(iterable,length)
+    result = len(iterable)**length
+
+    return result
+
 
 def pc(iterable,length):
+    """
+    Generate permutations of given iterable with specified length.
+
+    Parameters:
+        iterable (Iterable): The iterable object.
+        length (int): The length for permutation computation.
+
+    Returns:
+        list: List of permutations of the given iterable with the specified length.
+    """
     power = area(iterable,length)
     temp_list = [[None]*length for i in range(power)]
-    for i in range(-1,length*-1-1,-1): # negative i
-        change = len(iterable)**abs(i+1)
-        counter = 0 
+
+    negative_index = range(-1,-length-1,-1)
+
+    for i in negative_index: # row (right to left)
+        change = pow(len(iterable),abs(i+1)) # Calculate the frequency of change based on current row position
         index = 0
-        for j in range(power):
-            if counter != change:
-                temp_list[j][i] = iterable[index]
-                counter += 1
-            else:
-                counter = 0
-                if index<len(iterable)-1:
-                    index += 1
-                else:
-                    index = 0
-                temp_list[j][i] = iterable[index]
-                counter += 1
-        result = temp_list
+
+        for j in range(power): # column
+            if j % change == 0 and j!=0: # Check if it's time to change the index based on the change frequency
+                index = (index + 1) % len(iterable) # Update the index for the iterable
+            temp_list[j][i] = iterable[index]
+
+    result = temp_list
+
     return result
